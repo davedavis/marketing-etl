47fc1317d123f07620bc707bc6bdc51769dfd9a2
Dave Davis <dave@davedavis.io>
2021-01-03 22:30:40 +0000

    Add proper docstrings to main.py

--- main.py
+++ main.py
@@ -13,15 +13,33 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-""" App that populates a database of paid media platform cost data on three dimensions.
-    Account, Campaign and Ad levels."""
-import argparse
-import time
 
+"""Paid media platform report pulling pipeline.
+
+This script pulls data from Google Ads, Microsoft Ads and Adobe Analytics
+And writes it to a custom database for later report generation.
+It is assumed that this will be run from the command line with the quarter
+passed as an argument. As the database tables are determined by these
+parameters, this script will not work in an IDE.
+
+This script requires that `SQLAlchemy` be installed within the Python
+environment you are running this script in.
+
+This file can also be imported as a module and contains the following
+functions:
+    * main - the main function of the script
+"""
+
+# Leave this import for app execution timing.
+import sys
+
+import dg_utils.timing
+
+# This module is required for custom quarter and weeks based on company fiscal year.
 import fiscalyear
+import argparse
 
 from dg_config.settingsfile import get_settings
-import dg_utils.timing
 from dg_date import daterange
 from dg_db.db_utils import init_db
 from dg_google import google_ads_report_builder
@@ -31,7 +49,14 @@ settings = get_settings()
 
 
 def main(quarter):
-    """ Main method that calls all the worker modules """
+    """Gets the reports from platforms and writes them to the appropriate
+       DB tables in the database defined in the settings.yaml file.
+
+    Args:
+        quarter (int): The fiscal quarter the reports should be pulled for.
+
+    """
+
     print('Tracker Running...')
     print(f"Running for quarter {quarter} ")
 
@@ -42,24 +67,25 @@ def main(quarter):
 
     # Set date range.
     print('Calculating date range for reports..')
-
     google_date_range = daterange.get_google_date_range(quarter)
     bing_date_range_start, bing_date_range_end = daterange.get_bing_date_range(quarter)
     print("Google Date Range is: ", google_date_range)
     print("Bing Date Range is: ", bing_date_range_start, bing_date_range_end)
 
     # Initialize the report retrieval flow. Stagger platforms & sleep for rate limiting.
+    # Start the Accounts report flow for all platforms.
     google_ads_report_builder.get_report(google_date_range, report_type="accounts")
     microsoft_ads_report_builder.get_report(bing_date_range_start, bing_date_range_end, report_type="accounts")
-    # time.sleep(10)
-    #
+
+    # Start the Campaigns report flow for all platforms.
     google_ads_report_builder.get_report(google_date_range, report_type="campaigns")
     microsoft_ads_report_builder.get_report(bing_date_range_start, bing_date_range_end, report_type="campaigns")
-    # time.sleep(10)
-    #
+
+    # Start the Search Ads report flow for all platforms.
     google_ads_report_builder.get_report(google_date_range, report_type="ads")
     microsoft_ads_report_builder.get_report(bing_date_range_start, bing_date_range_end, report_type="ads")
-    #
+
+    # Start the Shopping Ads report flow for all platforms.
     google_ads_report_builder.get_report(google_date_range, report_type="shopping")
     microsoft_ads_report_builder.get_report(bing_date_range_start, bing_date_range_end, report_type="shopping")
 
@@ -67,11 +93,12 @@ def main(quarter):
 # ToDo: Make args a global variable instead of accessing them directly in the model for dynamic table creation.
 if __name__ == "__main__":
     # Set up argparse and support reporting for previous quarter.
-    parser = argparse.ArgumentParser(description="Updates or backfills SEM platform reporting.")
+    parser = argparse.ArgumentParser(description="Updates or backfills database with SEM platform reporting.")
     parser.add_argument("-q", "--quarter",
                         type=int,
                         default=fiscalyear.FiscalQuarter.current().quarter,
-                        help="The quarter period as an integer. 1, 2, 3 or 4 which will be for the current fiscal year.")
+                        help="The quarter as an integer. 1, 2, 3 or 4 which will be for the current fiscal year.")
     args = parser.parse_args()
 
     main(args.quarter)
+
