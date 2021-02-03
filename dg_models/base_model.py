5516dcaa8c525c41255c00a197ae176fe686403b
Dave Davis <dave@davedavis.io>
2021-02-03 22:11:48 +0000

    Add support for remote database (DigitalOcean managed DB/MySQL)

--- dg_models/base_model.py
+++ dg_models/base_model.py
@@ -9,7 +9,16 @@ settings = settingsfile.get_settings()
 # Init the global base
 Base = declarative_base()
 # engine = create_engine(settings['db_connection_string'], echo=True)
-engine = create_engine(settings['db_connection_string'])
+
+# ssl_args is needed when connecting to SSL on DO or other provider.
+# ToDo: Create conditional here to switch between local and remote (no ssl args)
+
+if settings['database_provider'] == "digitalocean":
+    ssl_args = {'ssl_ca': settings['db_connection_cert']}
+    engine = create_engine(settings['db_connection_string'], connect_args=ssl_args)
+else:
+    engine = create_engine(settings['db_connection_string'])
+
 Base.metadata.bind = engine
 
 # Init the global session.
